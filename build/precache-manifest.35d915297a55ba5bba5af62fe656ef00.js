5c51bffb73630082d022fe1813abff21b214b539
jhwu <jhwu87@gmail.com>
2020-04-11 23:00:53 +0800

    Added build folder

---
+++ build/precache-manifest.35d915297a55ba5bba5af62fe656ef00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab529f961d491f2aa5cf1643792559a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b3fc1cab43d7ac3d0b5",
+    "url": "/static/css/main.c626c5c5.chunk.css"
+  },
+  {
+    "revision": "3012dac35b4dd5aa0f24",
+    "url": "/static/js/2.05fddbc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05fddbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3fc1cab43d7ac3d0b5",
+    "url": "/static/js/main.ac1b18dd.chunk.js"
+  },
+  {
+    "revision": "0aab72a34a61a0ec844f",
+    "url": "/static/js/runtime-main.2d8131db.js"
+  }
+]);
